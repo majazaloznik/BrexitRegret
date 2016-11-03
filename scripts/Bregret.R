1ad652d2f2a5f41105fbe61e819cd8b4548975a9
majazaloznik <maja.zaloznik@gmail.com>
2016-11-03 18:24:00 +0000

    added age group and region, messy not tested

--- scripts/Bregret.R
+++ scripts/Bregret.R
@@ -8,10 +8,34 @@ library(tidyr)
 # # Data was downloaded from http://www.britishelectionstudy.com/custom/uploads/2016/10/BES2015_W9_Panel_v1.0.zip
 # # Then manually opened in Stata and saved as a csv file, so that only the required variables can be imported.
 # bregret.all <- read.csv("data/BES2015_W9_Panel_v1.0/bregret.csv", nrows=1)
+
+## looking for vairables:
+# search.pattern <- "education"
+# names(bregret.all)[grep(search.pattern,names(bregret.all))]
+# grep(search.pattern,names(bregret.all))
+# 
 # stencil <- c(rep("NULL",3879))
-# stencil[c(18, 26, 2218, 2636, 3148, 3464,3531,3533, 3535, 3758, 3759)] <- NA
+# stencil[c(18, #  = wt_full_W6
+#           26, # = wt_full_W9
+#           2218, # = regretsIHaveAFewW6
+#           2636, # = euRefExpectationW7
+#           3148, # = euRefExpectationW8
+#           3464, # = econPersonalRetroW8
+#           3531, # = euRefTurnoutRetroW9
+#           3533, # = euRefVoteW9
+#           3535, # = regretsIHaveAFewW9
+#           3752, # = ageGroup
+#           3758, # = ns_secW1W2W3W4W5
+#           3759, # = ns_sec_analyticW1W2W3W4W5
+#           3762, # = gender
+#           3766, # = gor
+#           3767 # = education
+#           )] <- NA
+# 
 # bregret.raw <- read.csv("data/BES2015_W9_Panel_v1.0/bregret.csv", colClasses = stencil)
 # write.table(bregret.raw, file = "data/bregret.raw.csv")
+
+
 bregret.raw <- read.table( "../data/bregret.raw.csv")
 regrets <- xtabs(wt_full_W9~ regretsIHaveAFewW9 + euRefVoteW9 , data = bregret.raw)
 
